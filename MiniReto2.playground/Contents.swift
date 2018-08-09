a4a1f13196ec2ae274bcb0553544785a76f17a51
Juan <40907249+juanchistri@users.noreply.github.com>
2018-08-09 11:53:19 -0300

    Update Contents.swift
    Se proponen cambios en las variables de la función y la forma en la que se utilizan los casos dentro del Switch haciendo referencia a la enumeración y no al raw value. con esos cambios el Playground ejecuto el código correctamente.

--- MiniReto2.playground/Contents.swift
+++ MiniReto2.playground/Contents.swift
@@ -19,19 +19,31 @@ class Auto{
     
     func cambioDeVelocidad( ) -> ( actual : Int, velocidadEnCadena: String){
         
-        let now = self.velocidad.rawValue
-        let actual = velocidad.rawValue
-        let velocidadEnCadena = String(velocidad)
+        var actual: Int = 0 // deberia ser variable entera y con valor 0, cual es el equivalente a Apagado
+        var actual = velocidad.rawValue // Deberia ser variable
+        //let velocidadEnCadena = String(velocidad) // No deberia ir esta constante ya que esta como un valor de retorno
         
-        switch now{
-        case 20:
-            self.velocidad = Velocidades.VelocidadMedia
-        case 50:
-            self.velocidad = Velocidades.VelocidadAlta
-        case 120:
-            self.velocidad = Velocidades.VelocidadMedia
+        switch velociad { // Se reemplaza por la instancia de la enumeraciòn 
+            
+            // Se corrigen los casos ya que se estaba llendo a buscar el raw.value y no la enumeraciòn en si, cual luego se usa para crear la instancia de la enuymeraciòn dentro de la case auto.
+        case .Apagado:
+            actual = velocidad.rawValue
+            velocidadEnCadena = "Apagado"
+            velocidad = Velocidades.VelocidadBaja
+        case .VelocidadBaja:
+            actual = velocidad.rawValue
+            velocidadEnCadena = "Velocidad Baja"
+            velocidad = Velocidades.VelocidadMedia
+        case .VelocidadMedia:
+            actual = velocidad.rawValue
+            velocidadEnCadena = "Velociad Media"
+            velocidad = Velocidades.VelocidadAlta
+        case .VelocidadAlta:
+            actual = velocidad.rawValue
+            velocidadEnCadena = "Velociad Alta"
+            velocidad = Velocidades.VelocidadMedia
         default:
-            self.velocidad = Velocidades.VelocidadBaja
+           self.velocidad = Velocidades.Apagado
         }
         
         let resultado = (actual, velocidadEnCadena)
